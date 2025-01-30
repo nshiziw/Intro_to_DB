482d48007f8771cd25779b02cb05f8d022f88116
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-30 14:55:01 +0200

    books database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -33,7 +33,7 @@ CREATE TABLE Order_Details(
     order_detail_id PRIMARY KEY,
     order_id INT,
     book_id INT,
-    FOREIGN KEY(order_id) REFERENCES Orders,
-    FOREIGN KEY(book_id) REFERENCES Books,
+    FOREIGN KEY(order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY(book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 )
