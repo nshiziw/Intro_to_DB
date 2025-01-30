355c7dabf5c875ee598d68b7a8af7326cae2fd30
nshizirungu wilson <wilsonshizirungu@gmail.com>
2025-01-30 15:03:27 +0200

    development

--- MySQLServer.py
+++ MySQLServer.py
@@ -8,13 +8,13 @@ my_db = mysql.connector.connect(
 
 my_cursor = my_db.cursor()
 
-my_cursor.execute("CREATE DATABASE IF NOT EXISTS my_database")
+my_cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
 # display message if database exists
 
 my_cursor.execute("SHOW DATABASES")
 for db in my_cursor:
-    if db[0] == "my_database":
-        print(f"Database 'my_database' already exists.")
+    if db[0] == "alx_book_store":
+        print(f"Database 'alx_book_store' already exists.")
         break
 
 print("Database 'alx_book_store' created successfully!")
